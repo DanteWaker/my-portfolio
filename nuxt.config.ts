524b87f9077f0b83013c00b84ae7faa7e4b56448
Denner Nascimento <denner.nascimento@protonmail.com>
2024-07-29 10:01:12 -0300

    chore: Update image provider configuration in nuxt.config.ts
    Remove the unused image provider configuration in nuxt.config.ts to clean up the codebase and improve maintainability.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,10 @@ export default defineNuxtConfig({
 		},
 	},
 	modules: ['@nuxt/ui', '@nuxt/eslint', 'nuxt-icon', '@nuxt/fonts', '@nuxt/image'],
+	image: {
+		provider: 'netlify',
+		domains: ['devbydenner.com'],
+	},
 	fonts: {
 		defaults: {
 			weights: [100, 200, 300, 400, 500, 600, 700, 800, 900, 950],
